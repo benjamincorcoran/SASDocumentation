f09b090fd14a9b48868f2e51479accb5f47370ec
Ben <b.corcoran@hefce.ac.uk>
2019-03-15 11:46:06 +0000

    Rename file, add minor documetation

--- buildDocumentation.bat
+++ buildDocumentation.bat
@@ -1,9 +1,8 @@
-@ECHO OFF
 
+@echo off
 pushd %~dp0
 
-REM Command file for Sphinx documentation
-if "%1" == "" goto end
+
 
 call sphinx-quickstart -q --project=%1 --author=%username% --batchfile --template=sphinxTemplate %1/docs
 python SASParser.py %1
