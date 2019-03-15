a45b6b5fe17b71e05879486f944828c22af2c716
Ben <b.corcoran@hefce.ac.uk>
2019-03-15 11:59:11 +0000

    Add python requirements to batch

--- buildDocumentation.bat
+++ buildDocumentation.bat
@@ -1,8 +1,9 @@
 
+
 @echo off
 pushd %~dp0
 
-
+pip install sphinx recommonmark sphinx-markdown-tables
 
 call sphinx-quickstart -q --project=%1 --author=%username% --batchfile --template=sphinxTemplate %1/docs
 python SASParser.py %1
