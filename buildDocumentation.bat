931b1091e8b66014318a193ce80ade6da51ef4ac
Ben <b.corcoran@hefce.ac.uk>
2019-03-15 16:05:31 +0000

    Add CSS styles to documentation

--- buildDocumentation.bat
+++ buildDocumentation.bat
@@ -7,6 +7,12 @@ pip install sphinx recommonmark sphinx-markdown-tables
 
 call sphinx-quickstart -q --project=%1 --author=%username% --batchfile --template=sphinxTemplate %1/docs
 python SASParser.py %1
+
+xcopy sphinxStatic %1\docs\_static\ /s/e/y
+
 %1/docs/make.bat html
+
+
 :end
 popd
+
