c0300aea1e6a85fc60dd3d5b63f5c0306ca2f5c0
Ben <ben.corcoran@officeforstudents.org.u>
2019-05-17 11:29:58 +0100

    Dynamically build library tests

--- SASDocumentation/SASObjects/tests/test_SASBaseObject.py
+++ SASDocumentation/SASObjects/tests/test_SASBaseObject.py
@@ -7,60 +7,47 @@ testSASBaseObject = SASBaseObject()
 
 # Split Data Objects
 
-splitDataObjectsTestPlan = {
+baseDataObjectsTestPlan = {
     'singleDataItem':('dataItem',[['','dataItem']]),
     'macroDataItem':('&dataItem.',[['','&dataItem.']]),
     'partialMacroDataItem':('&data.Item',[['','&data.Item']]),
     'parenthDataItem':('dataItem()',[['','dataItem()']]),
     'parenthConditionDataItem':('dataItem(where=() keep=test test)',[['','dataItem(where=() keep=test test)']]),
-    'parenthMacroDataItem':('&data.Item(where=())',[['','&data.Item(where=())']]),
-   
-    'singleLibDataItem':('library.dataItem',[['library','dataItem']]),
-    'macroLibDataItem':('library.&dataItem.',[['library','&dataItem.']]),
-    'partialLibMacroDataItem':('library.&data.Item',[['library','&data.Item']]),
-    'parenthLibDataItem':('library.dataItem()',[['library','dataItem()']]),
-    'parenthConditionLibDataItem':('library.dataItem(where=() keep=test test)',[['library','dataItem(where=() keep=test test)']]),
-    'parenthLibMacroDataItem':('library.&data.Item(where=())',[['library','&data.Item(where=())']]),
-
-    'macroLibSingleDataItem':('&library..dataItem',[['&library.','dataItem']]),
-    'macroLibMacroDataItem':('&library..&dataItem.',[['&library.','&dataItem.']]),
-    'macroLibPartialMacroDataItem':('&library..&data.Item',[['&library.','&data.Item']]),
-    'parenthMacroLibDataItem':('&library..dataItem()',[['&library.','dataItem()']]),
-    'parenthConditionMacroLibDataItem':('&library..dataItem(where=() keep=test test)',[['&library.','dataItem(where=() keep=test test)']]),
-    'parenthMacroLibPartialMacroDataItem':('&library..&data.Item(where=())',[['&library.','&data.Item(where=())']]),
-
-    'partialMacroLibDataItem':('li&bra.ry.dataItem',[['li&bra.ry','dataItem']]),
-    'partialMacroLibMacroDataItem':('li&bra.ry.&dataItem.',[['li&bra.ry','&dataItem.']]),
-    'partialMacroLibPartialMacroDataItem':('li&bra.ry.&data.Item',[['li&bra.ry','&data.Item']]),
-    'parenthPartialMacroLibDataItem':('li&bra.ry.dataItem()',[['li&bra.ry','dataItem()']]),
-    'parenthConditionPartialMacroLibDataItem':('li&bra.ry.dataItem(where=() keep=test test)',[['li&bra.ry','dataItem(where=() keep=test test)']]),
-    'parenthPartialMacroLibPartialMacroDataItem':('li&bra.ry.&data.Item(where=())',[['li&bra.ry','&data.Item(where=())']])
+    'parenthMacroDataItem':('&data.Item(where=())',[['','&data.Item(where=())']]),   
 }
 
+splitDataObjectsTestPlan = {}
 
-baseTestIds = []
-baseScenarios = []
+for id, test in baseDataObjectsTestPlan.items():
+    splitDataObjectsTestPlan[id]=test
+    splitDataObjectsTestPlan['{}Lib'.format(id)]=('library.{}'.format(test[0]),[['library',test[1][0][1]]])
+    splitDataObjectsTestPlan['{}MacroLib'.format(id)]=('&library..{}'.format(test[0]),[['&library.',test[1][0][1]]])
+    splitDataObjectsTestPlan['{}MacroPartialLib'.format(id)]=('li&bra.ry.{}'.format(test[0]),[['li&bra.ry',test[1][0][1]]])
+
+
+singleTestIds = []
+singleScenarios = []
 for id, scenario in splitDataObjectsTestPlan.items():
-    baseTestIds.append(id)
-    baseScenarios.append(scenario)
+    singleTestIds.append(id)
+    singleScenarios.append(scenario)
 
-@pytest.mark.parametrize("str,expected",baseScenarios,ids=baseTestIds)
+@pytest.mark.parametrize("str,expected",singleScenarios,ids=singleTestIds)
 def test_SASBaseObjectSplitSingleDataObject(str,expected):
     assert testSASBaseObject.splitDataObjects(str) == expected
 
-dynamicSplitDataObjectsTestPlan = {}
+multipleSplitDataObjectsTestPlan = {}
 
 for firstId, firstTest in splitDataObjectsTestPlan.items():
     for secondId, secondTest in splitDataObjectsTestPlan.items():
-        dynamicSplitDataObjectsTestPlan['{} & {}'.format(firstId,secondId)] = ('{} {}'.format(firstTest[0],secondTest[0]),[firstTest[1][0],secondTest[1][0]])
+        multipleSplitDataObjectsTestPlan['{} & {}'.format(firstId,secondId)] = ('{} {}'.format(firstTest[0],secondTest[0]),[firstTest[1][0],secondTest[1][0]])
 
-dynamicTestIds = []
-dynamicScenarios = []
-for id, scenario in dynamicSplitDataObjectsTestPlan.items():
-    dynamicTestIds.append(id)
-    dynamicScenarios.append(scenario)
+multipleTestIds = []
+multipleScenarios = []
+for id, scenario in multipleSplitDataObjectsTestPlan.items():
+    multipleTestIds.append(id)
+    multipleScenarios.append(scenario)
 
-@pytest.mark.parametrize("str,expected",dynamicScenarios,ids=dynamicTestIds)
+@pytest.mark.parametrize("str,expected",multipleScenarios,ids=multipleTestIds)
 def test_SASBaseObjectSplitMultipleDataObject(str,expected):
     assert testSASBaseObject.splitDataObjects(str) == expected
 
