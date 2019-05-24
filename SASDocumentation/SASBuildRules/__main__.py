74fa733cebebb5c4f711ef10989b66c24d6fc527
Ben <ben.corcoran@officeforstudents.org.u>
2019-05-24 15:27:24 +0100

    Testing print for ado

--- SASDocumentation/SASBuildRules/__main__.py
+++ SASDocumentation/SASBuildRules/__main__.py
@@ -5,5 +5,7 @@ from .camelCase import ruleCamelCase
 
 if __name__ == "__main__":
     path = '.'
+    print(path)
     prj = SASProject(path)
+    print(prj)
     ruleCamelCase(prj, strict=True)
--- SASDocumentation/SASBuildRules/camelCase.py
+++ SASDocumentation/SASBuildRules/camelCase.py
@@ -3,7 +3,6 @@
 
 import re
 import sys
-import logging
 
 class ruleCamelCase(object):
 
@@ -12,22 +11,15 @@ class ruleCamelCase(object):
         self.regex = r'[a-z]+[A-Z0-9][a-z0-9]+[A-Za-z0-9]*|^[a-z]+$'
         self.strict = strict
 
-        self.logger = logging.getLogger('Logger')
-        self.logger.setLevel(logging.DEBUG)
-
-        self.ch = logging.StreamHandler()
-        self.ch.setLevel(logging.DEBUG)
-        self.logger.addHandler(self.ch)
-
         failures = dict()
         for SASProgram in SASProject.SASPrograms:
             results = self.assess(SASProgram)
            
             if len(results)!=0:
                 failures[SASProgram]=results
-                self.logger.warning("##vso[task.logissue type=warning]Warning: camelCase not followed for {} in {}".format(results,SASProgram.name))
+                print("##vso[task.logissue type=warning]Warning: camelCase not followed for {} in {}".format(results,SASProgram.name))
         if len(failures) != 0:
-            self.logger.warning("##vso[task.complete result=SucceededWithIssues] SAS Build Rule: camelCase")
+            print("##vso[task.complete result=SucceededWithIssues] SAS Build Rule: camelCase")
 
     def assess(self, SASProgram):
         failures = []
