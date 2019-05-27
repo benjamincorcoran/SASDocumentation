5446f44f7580bbd2165a0e885131bef5b7ee8cec
Ben <ben.corcoran@officeforstudents.org.u>
2019-05-27 11:47:32 +0100

    Added SAS logging to generate error messages

--- SASDocumentation/SASBuildRules/__main__.py
+++ SASDocumentation/SASBuildRules/__main__.py
@@ -1,13 +1,33 @@
 
 import sys
+import logging
+
 from SASDocumentation.SASObjects.SASProject import SASProject
+
+
 from .camelCase import ruleCamelCase
+
+
+
 
 if __name__ == "__main__":
     
     mode = sys.argv[1]
     path = '.'
 
+    log = logging.getLogger('SASBuildRules')
+    log.setLevel(logging.DEBUG)
+
+    console = logging.StreamHandler()
+    consoleFmt = logging.Formatter('%(levelname)s - %(message)s %(additional)s')
+    console.setFormatter(consoleFmt)
+    log.addHandler(console)
+
+    ado = logging.StreamHandler()
+    adoFmt = logging.Formatter('##vso[%(adotags)s]%(levelname)s: %(message)s')
+    ado.setFormatter(adoFmt)  
+    log.addHandler(ado)
+
     prj = SASProject(path)
 
-    ruleCamelCase(prj, mode=mode)
+    ruleCamelCase(prj, log, mode=mode)
--- SASDocumentation/SASBuildRules/camelCase.py
+++ SASDocumentation/SASBuildRules/camelCase.py
@@ -6,23 +6,52 @@ import sys
 
 class ruleCamelCase(object):
 
-    def __init__(self, SASProject, mode='normal'):
+    def __init__(self, SASProject, logger, mode='normal'):
+
+        self.logger = logger
+        self.mode = mode
+        self.isStrict = self.mode=='strict'
+
+        if self.isStrict:
+            self.adoLevel = 'error'
+        else:
+            self.adoLevel = 'warning'
 
         self.regex = r'[a-z]+[A-Z0-9][a-z0-9]+[A-Za-z0-9]*|^[a-z]+$'
-        self.mode = mode
 
         failures = dict()
+        errors = 0
         for SASProgram in SASProject.SASPrograms:
             results = self.assess(SASProgram)
            
             if len(results)!=0:
                 failures[SASProgram]=results
-                print("##vso[task.logissue type=warning]Warning: camelCase not followed for {} in {}".format(results,SASProgram.name))
-        if len(failures) != 0 and self.mode =='normal':
-            print("##vso[task.complete result=SucceededWithIssues] SAS Build Rule: camelCase")
-        elif len(failures) != 0 and self.mode =='strict':
-            print("##vso[task.complete result=Failed] SAS Build Rule: camelCase")
+                errors += len(results)
+                for dataObject in results:
+                    self.logError(SASProgram.name,dataObject.startLine,dataObject=dataObject.id)
+
+        self.logRuleResult(errors)
  
+    def logError(self,path,lineNumber,dataObject=''):
+        adotags = 'task.logissue type={};sourcepath={};linenumber={}'.format(self.adoLevel,path,lineNumber)
+        additional = ': \t{} [Line: {}]'.format(path,lineNumber)
+        info = dict(adotags=adotags,additional=additional)
+        self.logger.warning('camelCase requirement failed for "{}"'.format(dataObject),extra=info)
+
+    def logRuleResult(self,errors):
+        if self.isStrict and errors>0:
+            result = 'Failed'
+        elif errors>0:
+            result = 'SucceededWithIssues'
+        else:
+            result = 'Succeeded'
+
+        adotags = 'task.complete result={}'.format(result)
+        info = dict(adotags=adotags,additional='')
+        self.logger.info('SAS Build Rule "camelCase": {} - {} issues'.format(result,errors),extra=info)
+
+
+
     def assess(self, SASProgram):
         failures = []
         for item in SASProgram.inputs+SASProgram.outputs:
