b24c6558dbf47fb0d8c1a93193a89cf288e39dd7
Ben <ben.corcoran@officeforstudents.org.uk>
2019-06-28 15:32:59 +0100

    Handle malformed data steps

--- SASDocumentation/SASObjects/tests/testHarness.py
+++ SASDocumentation/SASObjects/tests/testHarness.py
@@ -2,5 +2,5 @@
 
 from SASDocumentation.SASObjects.SASProgram import SASProgram
 
-file = r'S:\Allocations\Archive\2017-18 allocations archive\JulyAnnouncement2017\Grant tables\Code\July 2017-18 Grant tables.sas'
+file = r'S:\Allocations\NCOP funding\Queries and analysis\Review_Sept2018\SAS_Code\Development_work\NCOP_plus_v1.sas'
 test = SASProgram(file)
