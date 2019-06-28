38ae5e05f3afb8399ed66bc35f2769629f3cc1f9
Ben <ben.corcoran@officeforstudents.org.uk>
2019-06-28 15:37:22 +0100

    Handle malformed data stepsC:/Miniconda3/python.exe

--- SASDocumentation/SASObjects/tests/testHarness.py
+++ SASDocumentation/SASObjects/tests/testHarness.py
@@ -2,5 +2,5 @@
 
 from SASDocumentation.SASObjects.SASProgram import SASProgram
 
-file = r'S:\Allocations\NCOP funding\Queries and analysis\Review_Sept2018\SAS_Code\Development_work\NCOP_plus_v1.sas'
+file = r'S:\Allocations\Student premium\Pre-2016 L drive apps folders\Apps09_\WPIR10_\PCOMP10$\2010-11 flexible study measure weighting - 07052010.sas'
 test = SASProgram(file)
